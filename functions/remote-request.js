4a846b88b9a3433db2ffec257dc1116e9c3e4f3f
clouder300 <clouder300@clouder300.de>
2023-12-01 17:00:17 +0100

    remote-request fix

--- functions/remote-request.js
+++ functions/remote-request.js
@@ -1,6 +1,6 @@
 export async function onRequest(context) {
     const { searchParams } = new URL(context.url)
-    const url = searchParams.get('url')
+    const url = decodeURI(searchParams.get('url'))
 
     const response = await fetch(url)
     return new Response(response.text())
